476c1593ed8d76e58cd2bd1931eb60f26c5c7f21
James Gillmore <james@faceyspacey.com>
2015-12-10 17:35:45 -0800

    change to test #with

--- test.js
+++ test.js
@@ -1,15 +1,29 @@
 if(Meteor.isServer) return;
 
+Template.TestContainer.onCreated(function() {
+  this.foo = 123;
+});
+
+Template.TestContainer.events({
+  'click .test': function(e, t) {
+    console.log('TEMPLATE', this, t.foo)
+  }
+});
+
 Template.TestContainer.helpers({
   getUser: function() {
     var name = Session.get("name") || "James";
+    return {name: name};
+  },
+  getDog: function() {
+    var name = Session.get("dog") || "Lucy";
     return {name: name};
   }
 });
 
 Template.Test.helpers({
   getName: function() {
-    return this.user.name;
+    return this.name;
   }
 });
 
@@ -17,6 +31,6 @@ Template.Test.onCreated(function() {
   console.log('CREATED');
 });
 
- Template.Test.onRendered(function() {
+Template.Test.onRendered(function() {
   console.log('RENDERED'); //yes, i also did the inspect element + manually change text test too
 });
